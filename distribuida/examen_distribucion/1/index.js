56e3a60eff693c2daf8448e96384cc80f3661c5a
“[name]” <g5ada02@hackademy.mx>
2021-04-15 18:02:36 -0500

    first commit

--- distribuida/examen_distribucion/1/index.js
+++ distribuida/examen_distribucion/1/index.js
@@ -1,18 +1,7 @@
 //Fetch API
 const fetch = require("node-fetch");
-/*
-Fetch API (limite 100):
-*/
 
-   fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200').then(res => res.json())
-                                                       .then(data =>{
-                                                             console.log(data.results)
-                                                             const nameArr = data.results.map (pokemon => pokemon.name)
-                                                             console.log(nameArr) })
-                                                        .catch(e => console.error(e))
-/*
-Async Await (limite 100):
-*/
+
    const pokemonName = async() =>
     {
        try {
